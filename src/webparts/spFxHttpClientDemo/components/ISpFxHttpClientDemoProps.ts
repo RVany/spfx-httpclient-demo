19d1a957fb5ed22ef5a31948f340c2df07f3b15c
MoonSky1! <vanmathyrr@gmail.com>
2023-12-29 10:20:13 -0500

    first commit

---
+++ src/webparts/spFxHttpClientDemo/components/ISpFxHttpClientDemoProps.ts
@@ -0,0 +1,7 @@
+export interface ISpFxHttpClientDemoProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
